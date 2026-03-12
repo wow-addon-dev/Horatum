862fb9fc5bbd7ec84a6fc15a2d86a6e303041626
Dionys <72759855+diomsg-code@users.noreply.github.com>
2026-03-12 13:30:27 +0100

    v1.13

--- data/CoreData.lua
+++ data/CoreData.lua
@@ -5,7 +5,6 @@ HRT.ADDON_VERSION = C_AddOns.GetAddOnMetadata(addonName, "Version")
 HRT.ADDON_BUILD_DATE = C_AddOns.GetAddOnMetadata(addonName, "X-BuildDate")
 
 HRT.GAME_VERSION = GetBuildInfo()
-HRT.GAME_FLAVOR = C_AddOns.GetAddOnMetadata(addonName, "X-Flavor")
 
 HRT.LINK_GITHUB = C_AddOns.GetAddOnMetadata(addonName, "X-Github")
 HRT.LINK_CURSEFORGE = C_AddOns.GetAddOnMetadata(addonName, "X-Curseforge")
@@ -18,3 +17,15 @@ HRT.GAME_TYPE_TBC = (WOW_PROJECT_ID == WOW_PROJECT_BURNING_CRUSADE_CLASSIC)
 ---@diagnostic disable-next-line: undefined-global
 HRT.GAME_TYPE_MISTS = (WOW_PROJECT_ID == WOW_PROJECT_MISTS_CLASSIC)
 HRT.GAME_TYPE_MAINLINE = (WOW_PROJECT_ID == WOW_PROJECT_MAINLINE)
+
+HRT.GAME_FLAVOR = "unknown"
+
+if HRT.GAME_TYPE_VANILLA then
+	HRT.GAME_FLAVOR = "Classic"
+elseif HRT.GAME_TYPE_TBC then
+	HRT.GAME_FLAVOR = "Burning Crusade - Classic Anniversary Edition"
+elseif HRT.GAME_TYPE_MISTS then
+	HRT.GAME_FLAVOR = "Mist of Pandaria - Classic"
+elseif HRT.GAME_TYPE_MAINLINE then
+	HRT.GAME_FLAVOR = "Retail"
+end
